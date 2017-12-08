105a9daf359bfc1292660518f5151fde9e2f542d
StrikerElite <strikerelite@outlook.hu>
2017-12-08 21:58:29 +0100

    Camera Movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
     public float CameraSpeed;
+    public float LeftBorder;
+    public float RightBorder;
+    public float UpperBorder;
+    public float BottomBorder;
+
+    void Start() {
+        MapGen mg = GameObject.Find("GameManager").GetComponent<MapGen>();
+        LeftBorder = 0;
+        RightBorder = mg.MapSize.x;
+        UpperBorder = mg.MapSize.y - 2;
+        BottomBorder = -3;
+    }
 
     void Update() {
-
-        if (Input.mousePosition.x <= 0 + Screen.currentResolution.width / 96) transform.position += Vector3.left * CameraSpeed;
-        if (Input.mousePosition.x >= Screen.currentResolution.width - Screen.currentResolution.width / 96) transform.position += Vector3.right * CameraSpeed;
-        Debug.Log(Input.mousePosition.x);
-        Debug.Log(Screen.currentResolution.width);
+        if (transform.position.x > LeftBorder) 
+            if (Input.GetKey("a")) transform.position += Vector3.left * CameraSpeed * Time.deltaTime * 50f;
+        if (transform.position.x < RightBorder)
+            if (Input.GetKey("d")) transform.position += Vector3.right * CameraSpeed * Time.deltaTime * 50f;
+        if (transform.position.z > BottomBorder)
+            if (Input.GetKey("s")) transform.position += Vector3.back * CameraSpeed * Time.deltaTime * 50f;
+        if (transform.position.z < UpperBorder)
+            if (Input.GetKey("w")) transform.position += Vector3.forward * CameraSpeed * Time.deltaTime * 50f; 
     }
 }

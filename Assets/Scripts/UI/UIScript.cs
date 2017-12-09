ea7736ec1efb2dd8f526fcfcac82738a4761f61c
StrikerElite <strikerelite@outlook.hu>
2017-12-09 15:41:56 +0100

    Minor UI progress

--- Assets/Scripts/UI/UIScript.cs
+++ Assets/Scripts/UI/UIScript.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIScript : MonoBehaviour {
-
+public class UIScript : MonoBehaviour { 
     void Start() {
         MapGen mg = GameObject.Find("GameManager").GetComponent<MapGen>();
-        GetComponent<RectTransform>().sizeDelta = new Vector2(mg.MapSize.x+2, mg.MapSize.y + 2);
 	}
 }
